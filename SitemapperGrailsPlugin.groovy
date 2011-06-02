04bc0b8487b4a8854ea3884ebfa4ef472245e6b8
Kim A. Betti <kim.betti@gmail.com>
2011-06-02 12:15:32 +0200

    Bumped version

--- SitemapperGrailsPlugin.groovy
+++ SitemapperGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class SitemapperGrailsPlugin {
 	
-    def version = "0.4"
+    def version = "0.5"
     def grailsVersion = "1.3.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
