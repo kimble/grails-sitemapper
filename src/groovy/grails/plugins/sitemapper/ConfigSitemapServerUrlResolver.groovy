19caccf7ae371812b713a790513f0282e8c11d09
Kim A. Betti <kim.betti@gmail.com>
2011-06-02 12:15:50 +0200

    Removed ConfigurationHolder

--- src/groovy/grails/plugins/sitemapper/ConfigSitemapServerUrlResolver.groovy
+++ src/groovy/grails/plugins/sitemapper/ConfigSitemapServerUrlResolver.groovy
@@ -3,6 +3,8 @@ package grails.plugins.sitemapper
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.groovy.grails.commons.ConfigurationHolder;
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+import org.codehaus.groovy.grails.plugins.support.aware.GrailsApplicationAware;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
@@ -10,14 +12,15 @@ import org.springframework.beans.factory.InitializingBean;
  * Looks up server URL in application configuration (grails.serverURL). 
  * @author Kim A. Betti
  */
-class ConfigSitemapServerUrlResolver implements SitemapServerUrlResolver, InitializingBean {
+class ConfigSitemapServerUrlResolver implements SitemapServerUrlResolver, InitializingBean, GrailsApplicationAware {
 	
 	private static final Log log = LogFactory.getLog(this)
 	
+    GrailsApplication grailsApplication
 	String serverUrl = null
 	
 	void afterPropertiesSet() {
-		serverUrl = ConfigurationHolder.config?.grails?.serverURL?.toString()
+        serverUrl = grailsApplication.config?.grails?.serverURL?.toString()
 		if (!serverUrl) {
 			log.error ("Unable to find server url, please set grails.serverURL in Config.groovy, "
 				+ "or provided your own implementation of SitemapServerUrlResolver.")
