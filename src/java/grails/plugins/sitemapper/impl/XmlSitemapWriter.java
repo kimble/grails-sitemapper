b91100aa60d3b84de12492582a5e6f206017245f
Kim A. Betti <kim.betti@gmail.com>
2011-06-21 08:19:11 +0200

    Don't add sitemaps without a previous modification date to the index sitemap

--- src/java/grails/plugins/sitemapper/impl/XmlSitemapWriter.java
+++ src/java/grails/plugins/sitemapper/impl/XmlSitemapWriter.java
@@ -5,83 +5,88 @@ import grails.plugins.sitemapper.Sitemapper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class XmlSitemapWriter extends AbstractSitemapWriter {
 
-  private final static String SITEMAP_OPEN = "<sitemap>";
-  private final static String SITEMAP_CLOSE = "</sitemap>\n";
+    private final static Logger log = LoggerFactory.getLogger(XmlSitemapWriter.class);
 
-  @Override
-  public void writeIndexEntries(PrintWriter writer) throws IOException {
-    writeIndexHead(writer);
+    private final static String SITEMAP_OPEN = "<sitemap>";
+    private final static String SITEMAP_CLOSE = "</sitemap>\n";
 
-    SitemapDateUtils dateUtils = new SitemapDateUtils();
-    String serverUrl = serverUrlResolver.getServerUrl();
-    for (String mapperName : sitemappers.keySet()) {
-      Sitemapper mapper = sitemappers.get(mapperName);
-      String lastMod = dateUtils.formatForSitemap(mapper.getPreviousUpdate());
+    @Override
+    public void writeIndexEntries(PrintWriter writer) throws IOException {
+        writeIndexHead(writer);
 
-      if (mapper instanceof PaginationSitemapper) {
-        PaginationSitemapper paginationMapper = (PaginationSitemapper) mapper;
+        SitemapDateUtils dateUtils = new SitemapDateUtils();
+        String serverUrl = serverUrlResolver.getServerUrl();
+        for (String mapperName : sitemappers.keySet()) {
+            Sitemapper mapper = sitemappers.get(mapperName);
+            Date previousUpdate = mapper.getPreviousUpdate();
+            if (previousUpdate != null) {
+                String lastMod = dateUtils.formatForSitemap(previousUpdate);
 
-        int count = paginationMapper.getPagesCount();
-
-        for (int i = 0; i < count; i++) {
-          writeIndexExtry(writer, serverUrl, mapperName + "-" + i, lastMod);
+                if (mapper instanceof PaginationSitemapper) {
+                    PaginationSitemapper paginationMapper = (PaginationSitemapper) mapper;
+                    int count = paginationMapper.getPagesCount();
+                    for (int i = 0; i < count; i++) {
+                        writeIndexExtry(writer, serverUrl, mapperName + "-" + i, lastMod);
+                    }
+                } else {
+                    writeIndexExtry(writer, serverUrl, mapperName, lastMod);
+                }
+            } else {
+                log.debug("No entries found for " + mapperName);
+            }
         }
-      } else {
-        writeIndexExtry(writer, serverUrl, mapperName, lastMod);
-      }
+
+        writeIndexTail(writer);
     }
 
-    writeIndexTail(writer);
-  }
+    @Override
+    public void writeSitemapEntries(PrintWriter writer, Sitemapper sitemapper, Integer pageNumber) throws IOException {
+        writeSitemapHead(writer);
+        super.writeSitemapEntries(writer, sitemapper, pageNumber);
+        writeSitemapTail(writer);
+    }
 
-  @Override
-  public void writeSitemapEntries(PrintWriter writer, Sitemapper sitemapper, Integer pageNumber)
-      throws IOException {
-    writeSitemapHead(writer);
+    @Override
+    public void writeSitemapEntries(PrintWriter writer, Sitemapper mapper) throws IOException {
+        String serverUrl = serverUrlResolver.getServerUrl();
+        EntryWriter entryWriter = new XmlEntryWriter(writer, serverUrl);
+        mapper.withEntryWriter(entryWriter);
+    }
 
-    super.writeSitemapEntries(writer, sitemapper, pageNumber);
+    private void writeIndexHead(PrintWriter writer) {
+        writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        writer.print("\n");
+        writer.print("<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+        writer.print("\n");
+    }
 
-    writeSitemapTail(writer);
-  }
+    private void writeIndexTail(PrintWriter writer) {
+        writer.print("</sitemapindex>");
+    }
 
-  @Override
-  public void writeSitemapEntries(PrintWriter writer, Sitemapper mapper) throws IOException {
-    String serverUrl = serverUrlResolver.getServerUrl();
-    EntryWriter entryWriter = new XmlEntryWriter(writer, serverUrl);
-    mapper.withEntryWriter(entryWriter);
-  }
+    private void writeIndexExtry(PrintWriter writer, String serverUrl, String mapperName, String lastMod) throws IOException {
+        writer.print(SITEMAP_OPEN);
+        writer.print(String.format("<loc>%s/sitemap-%s.xml</loc>", serverUrl, mapperName));
+        writer.print(String.format("<lastmod>%s</lastmod>", lastMod));
+        writer.print(SITEMAP_CLOSE);
+    }
 
-  private void writeIndexHead(PrintWriter writer) {
-    writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-    writer.print("\n");
-    writer.print("<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
-    writer.print("\n");
-  }
+    private void writeSitemapHead(PrintWriter writer) {
+        writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        writer.print("\n");
+        writer.print("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+        writer.print("\n");
+    }
 
-  private void writeIndexTail(PrintWriter writer) {
-    writer.print("</sitemapindex>");
-  }
-
-  private void writeIndexExtry(PrintWriter writer, String serverUrl, String mapperName, String lastMod)
-      throws IOException {
-    writer.print(SITEMAP_OPEN);
-    writer.print(String.format("<loc>%s/sitemap-%s.xml</loc>", serverUrl, mapperName));
-    writer.print(String.format("<lastmod>%s</lastmod>", lastMod));
-    writer.print(SITEMAP_CLOSE);
-  }
-
-  private void writeSitemapHead(PrintWriter writer) {
-    writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-    writer.print("\n");
-    writer.print("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
-    writer.print("\n");
-  }
-
-  private void writeSitemapTail(PrintWriter writer) {
-    writer.print("</urlset>");
-  }
+    private void writeSitemapTail(PrintWriter writer) {
+        writer.print("</urlset>");
+    }
 
 }
